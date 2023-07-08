d154c7e95cea6a6d35dc6ac514bd24354921a569
kulklex <kulklex@gmail.com>
2023-07-09 00:29:14 +0100

    controller for fetching prompts by specific users

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,17 @@
+import { connectDB } from '@/utils/database'
+import Prompt from '@/models/prompt'
+
+export const GET = async (req, {params}) => {
+
+    try{
+        await connectDB()
+
+        const prompts = await Prompt.find({creator: params.id}).populate('creator')
+
+
+        return new Response(JSON.stringify(prompts), {status: 200})
+    } catch (error) {
+        console.error(error)
+        return new Response("Failed to fetch posts from user", {status: 500})
+    }
+}
